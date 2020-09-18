a96ca2c5c1338eb6137cda762eeb7bb51d02b938
arthur.telles <atelles311@gmail.com>
2020-09-17 22:21:48 -0300

    update

--- train.py
+++ train.py
@@ -168,7 +168,7 @@ def eval(model, criterion, dev_iter):
             dec_inputs_len = dec_inputs_len - 1
 
             dec_logits, *_ = model(enc_inputs, enc_inputs_len, dec_inputs, dec_inputs_len)
-            step_loss = criterion(dec_logits, dec_targets.contiguous().view(-1))
+            step_loss = criterion(dec_logits.cuda(), dec_targets.contiguous().view(-1).cuda())
             eval_loss_total += float(step_loss.data[0])
             n_words_total += torch.sum(dec_inputs_len)
             n_sents_total += dec_inputs_len.size(0)
